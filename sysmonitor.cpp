81f086873ddb3ee58a7cc0d63d58aca9c927fbad
Khalis Tofari <khalis25feb@gmail.com>
2026-05-04 20:51:09 +0700

    menambahkan mekanisme deteksi perhitungan usage cpu

--- sysmonitor.cpp
+++ sysmonitor.cpp
@@ -10,8 +10,36 @@ SystemMonitor::SystemMonitor(QObject *parent) : QObject(parent)
     updateStats();
 }
 
+uint64_t fileTimeToUint64(const FILETIME& ft) {
+    return (static_cast<uint64_t>(ft.dwHighDateTime) << 32) | ft.dwLowDateTime;
+}
+
 void SystemMonitor::updateStats()
 {
+    FILETIME idleTime, kernelTime, userTime;
+    if (GetSystemTimes(&idleTime, &kernelTime, &userTime)) {
+
+        uint64_t currentIdle = fileTimeToUint64(idleTime);
+        uint64_t currentKernel = fileTimeToUint64(kernelTime);
+        uint64_t currentUser = fileTimeToUint64(userTime);
+
+        // Hitung selisih antara T2 (sekarang) dan T1 (sebelumnya)
+        uint64_t idleDiff = currentIdle - m_prevIdleTime;
+        uint64_t kernelDiff = currentKernel - m_prevKernelTime;
+        uint64_t userDiff = currentUser - m_prevUserTime;
+        uint64_t totalDiff = kernelDiff + userDiff;
+
+        if (totalDiff > 0) {
+            // Rumus: 100% - (Waktu Idle / Total Waktu)
+            m_cpuValue = 100.0 * (1.0 - static_cast<double>(idleDiff) / totalDiff);
+            emit cpuValueChanged();
+        }
+
+        // Simpan data sekarang untuk perbandingan di detik berikutnya (T1 berikutnya)
+        m_prevIdleTime = currentIdle;
+        m_prevKernelTime = currentKernel;
+        m_prevUserTime = currentUser;
+    }
     // Mengambil data RAM menggunakan Windows API
     MEMORYSTATUSEX memInfo;
     memInfo.dwLength = sizeof(MEMORYSTATUSEX);
@@ -25,3 +53,4 @@ void SystemMonitor::updateStats()
         emit ramValueChanged(); // Kirim sinyal ke QML agar UI update
     }
 }
+
--- sysmonitor.h
+++ sysmonitor.h
@@ -8,6 +8,7 @@ class SystemMonitor : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(double ramValue READ ramValue NOTIFY ramValueChanged)
+    Q_PROPERTY(double cpuValue READ cpuValue NOTIFY cpuValueChanged)
 
 public:
     explicit SystemMonitor(QObject *parent = nullptr);
@@ -16,6 +17,7 @@ public:
 
 signals:
     void ramValueChanged();
+    void cpuValueChanged();
 
 private slots:
     void updateStats();
@@ -23,6 +25,14 @@ private slots:
 private:
     double m_ramValue = 0;
     QTimer *m_timer;
+
+private:
+    // Tambahkan variabel untuk menyimpan data T1 (waktu sebelumnya)
+    uint64_t m_prevIdleTime = 0;
+    uint64_t m_prevKernelTime = 0;
+    uint64_t m_prevUserTime = 0;
+
+    double m_cpuValue = 0; // Tambahkan properti untuk CPU
 };
 
 #endif // SYSTEMMONITOR_H
